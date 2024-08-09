ee3c4120bdb7b8f560fd10efad59be4215cb49bb
abdusamadzoidov17 <abdusamadzoidov17@gmail.com>
2024-08-09 19:09:54 +0500

    Initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2023 Weather App. All rights reserved. Created by <a href="https://abdusamad.web.app/" target="_blank">Abdusamad Zoidov</a></p>
+      <p>&copy; 2024 Weather App. All rights reserved. Created by <a href="https://abdusamad.web.app/" target="_blank">Abdusamad Zoidov</a></p>
     </footer>
   );
 };
